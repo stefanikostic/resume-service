e63d6a8f49d7930476bf315cb4ffb789ae6d514a
Stefani Kostic <stefanikostic6@gmail.com>
2025-02-08 15:12:05 +0100

    Added ResumeController.

--- src/main/java/com/elevateresume/resume_service/controller/ResumeController.java
+++ src/main/java/com/elevateresume/resume_service/controller/ResumeController.java
@@ -1,41 +1,47 @@
 package com.elevateresume.resume_service.controller;
 
-import com.elevateresume.resume_service.dto.Resume;
-import com.elevateresume.resume_service.repository.ResumeRepository;
-import com.elevateresume.resume_service.service.ResumeService;
+import com.elevateresume.resume_service.dto.ResumeUploadDataDTO;
+import com.elevateresume.resume_service.entity.Resume;
+import com.elevateresume.resume_service.service.interfaces.ResumeService;
+import com.elevateresume.resume_service.service.interfaces.StorageService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
+
+import java.io.IOException;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/resume")
+@RequiredArgsConstructor
 public class ResumeController {
 
     private final ResumeService resumeService;
+    private final StorageService storageService;
 
-    public ResumeController(ResumeService resumeService) {
-        this.resumeService = resumeService;
+    @GetMapping("/user")
+    public ResponseEntity<Resume> getResumeByUserId() throws IOException {
+        Optional<Resume> resumeOptional = resumeService.getResumeByUserId();
+        if (resumeOptional.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        Resume resume = resumeOptional.get();
+        String s3FileName = resume.getS3FileName();
+        String content = storageService.getLatestVersionOfResume(s3FileName);
+        return ResponseEntity.ok(resume);
     }
 
-    @GetMapping("/user/{userId}")
-    public Mono<ResponseEntity<Resume>> getResumeByUserId(@PathVariable String userId) {
-        return resumeService.getResumeByUserId(userId)
-                .map(ResponseEntity::ok)
-                .defaultIfEmpty(ResponseEntity.notFound().build());
-    }
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadResume(@RequestParam("resume") MultipartFile file) throws IOException {
+        ResumeUploadDataDTO resumeUploadDataDTO = storageService.uploadFile(file);
 
-    @GetMapping("/")
-    public Flux<ResponseEntity<Resume>> getResumesByTemplate() {
-        return resumeService.getResumesByTemplate("template")
-                .map(ResponseEntity::ok)
-                .defaultIfEmpty(ResponseEntity.notFound().build());
-    }
+        resumeService.createResume(resumeUploadDataDTO, file);
 
-    @PostMapping
-    public ResponseEntity<String> uploadResume(@RequestParam("resume") MultipartFile file) {
-        resumeService.mapResumeFileToResume(file);
-        return ResponseEntity.ok("Success");
+        return ResponseEntity.ok("Resume uploaded successfully: " + resumeUploadDataDTO);
     }
 }
