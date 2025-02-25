6ff05b801d08468f1befb6b49d5442888cb991fc
Stefani Kostic <stefanikostic6@gmail.com>
2025-02-25 21:17:38 +0100

    Enabled caching in main application class.

--- src/main/java/com/elevateresume/resume_service/ResumeServiceApplication.java
+++ src/main/java/com/elevateresume/resume_service/ResumeServiceApplication.java
@@ -2,8 +2,14 @@ package com.elevateresume.resume_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @SpringBootApplication
+@EnableCaching
+@EnableJpaRepositories(basePackages = "com.elevateresume.resume_service.repository.jpa")
+@EnableRedisRepositories(basePackages = "com.elevateresume.resume_service.repository.redis")
 public class ResumeServiceApplication {
 
 	public static void main(String[] args) {
