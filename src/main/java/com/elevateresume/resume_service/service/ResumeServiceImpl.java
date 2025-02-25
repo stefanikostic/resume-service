d019bbdaa9aafb58728066e1d774e65aea4a085b
Stefani Kostic <stefanikostic6@gmail.com>
2025-02-25 21:18:14 +0100

    Added business logic into resume service.

--- src/main/java/com/elevateresume/resume_service/service/ResumeServiceImpl.java
+++ src/main/java/com/elevateresume/resume_service/service/ResumeServiceImpl.java
@@ -1,17 +1,21 @@
 package com.elevateresume.resume_service.service;
 
-import com.elevateresume.resume_service.dto.BulletPointDTO;
+import com.elevateresume.resume_service.dto.BulletPointInsertDTO;
 import com.elevateresume.resume_service.dto.ResumeInsertDTO;
 import com.elevateresume.resume_service.dto.ResumeUploadDataDTO;
-import com.elevateresume.resume_service.dto.SectionDTO;
+import com.elevateresume.resume_service.dto.SectionInsertDTO;
 import com.elevateresume.resume_service.entity.BulletPoint;
 import com.elevateresume.resume_service.entity.Resume;
 import com.elevateresume.resume_service.entity.Section;
-import com.elevateresume.resume_service.repository.ResumeRepository;
+import com.elevateresume.resume_service.redis.model.ResumeRedisModel;
+import com.elevateresume.resume_service.repository.redis.ResumeRedisRepository;
+import com.elevateresume.resume_service.repository.jpa.ResumeJpaRepository;
 import com.elevateresume.resume_service.service.interfaces.ResumeService;
 import com.elevateresume.resume_service.util.RequestContext;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDateTime;
@@ -21,58 +25,99 @@ import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ResumeServiceImpl implements ResumeService {
 
-    private final ResumeRepository resumeRepository;
+    private final ResumeJpaRepository resumeJpaRepository;
+    private final ResumeRedisRepository resumeRedisRepository;
     private final RequestContext requestContext;
+    private final ResumeProcessor resumeProcessor;
+    private final ResumeMapper resumeMapper;
 
     @Override
-    public Optional<Resume> getResumeByUserId() {
-        String userId = requestContext.getUserId();
-        return resumeRepository.findByUserId(userId);
+    public ResumeRedisModel getResumeByUserId(String userId) {
+        log.info("Started get resume.");
+        Optional<ResumeRedisModel> resumeRedisModel = resumeRedisRepository.findByUserId(userId);
+        if (resumeRedisModel.isPresent()) {
+            log.debug("Resume fetched from cache.");
+            return resumeRedisModel.get();
+        }
+        log.debug("Resume fetched from DB.");
+
+        return getResumeFromDb(userId);
+    }
+
+    @Override
+    @Transactional
+    public ResumeRedisModel getResumeFromDb(String userId) {
+        Optional<Resume> resume = resumeJpaRepository.findByUserId(userId);
+        return resume.map(resumeMapper::toResumeRedisModel).orElse(null);
     }
 
     @Override
     public List<Resume> getResumesByTemplate(String template) {
-        return resumeRepository.findByTemplate(template);
+        return resumeJpaRepository.findByTemplate(template);
     }
 
     @Override
     public Resume createResume(ResumeInsertDTO resumeInsertDTO) {
         Resume resume = new Resume();
-        resume.setCreatedAt(LocalDateTime.now());
-        resume.setTitle(resumeInsertDTO.title());
-        for (SectionDTO sectionDTO : resumeInsertDTO.sections()) {
-            Section section = new Section();
-            section.setTitle(sectionDTO.title());
-            section.setContent(sectionDTO.content());
-            section.setStartPeriod(sectionDTO.startPeriod());
-            section.setEndPeriod(sectionDTO.endPeriod());
-            section.setType(sectionDTO.sectionType());
-            List<BulletPoint> bulletPoints = new ArrayList<>();
-            for (BulletPointDTO bulletPointDTO : sectionDTO.bulletPoints()) {
-                BulletPoint bulletPoint = new BulletPoint();
-                bulletPoint.setTitle(bulletPointDTO.title());
-                bulletPoint.setContent(bulletPointDTO.content());
-                bulletPoints.add(bulletPoint);
-            }
-            section.setBulletPoints(bulletPoints);
 
-            resume.addSection(section);
-        }
-
-        return resumeRepository.save(resume);
+        return resumeJpaRepository.save(resume);
     }
 
     @Override
-    public void createResume(ResumeUploadDataDTO resumeUploadDataDTO, MultipartFile file) {
+    public ResumeRedisModel createResume(ResumeUploadDataDTO resumeUploadDataDTO, MultipartFile file) {
+        ResumeInsertDTO resumeInsertDTO = resumeProcessor.processResume();
         Resume resume = new Resume();
+
         resume.setFileName(file.getOriginalFilename());
         resume.setS3Link(resumeUploadDataDTO.fileUrl());
         resume.setS3FileName(resumeUploadDataDTO.fileName());
 
+        resume.setCreatedAt(LocalDateTime.now());
+        resume.setTitle(resumeInsertDTO.title());
+        if (resumeInsertDTO.sections() != null) {
+            for (SectionInsertDTO sectionInsertDTO : resumeInsertDTO.sections()) {
+                Section section = buildSection(sectionInsertDTO);
+
+                resume.addSection(section);
+            }
+        }
         resume.setUserId(requestContext.getUserId());
 
-        resumeRepository.save(resume);
+        Resume savedResume = save(resume);
+        log.debug("Resume persisted in DB.");
+
+        ResumeRedisModel resumeRedisModel = resumeMapper.toResumeRedisModel(savedResume);
+        resumeRedisRepository.save(resumeRedisModel);
+        log.debug("Resume persisted in cache.");
+        return resumeRedisModel;
+    }
+
+
+    private Resume save(Resume resume) {
+        return resumeJpaRepository.save(resume);
+    }
+
+    private static Section buildSection(SectionInsertDTO sectionInsertDTO) {
+        Section section = new Section();
+        section.setTitle(sectionInsertDTO.title());
+        section.setContent(sectionInsertDTO.content());
+        section.setStartPeriod(sectionInsertDTO.startPeriod());
+        section.setEndPeriod(sectionInsertDTO.endPeriod());
+        section.setType(sectionInsertDTO.sectionType());
+        List<BulletPoint> bulletPoints = new ArrayList<>();
+        if (sectionInsertDTO.bulletPoints() != null) {
+            for (BulletPointInsertDTO bulletPointInsertDTO : sectionInsertDTO.bulletPoints()) {
+                BulletPoint bulletPoint = new BulletPoint();
+                bulletPoint.setTitle(bulletPointInsertDTO.title());
+                bulletPoint.setSection(section);
+                bulletPoint.setContent(bulletPointInsertDTO.content());
+                bulletPoints.add(bulletPoint);
+            }
+        }
+        section.setBulletPoints(bulletPoints);
+        return section;
     }
 }
--- src/main/java/com/elevateresume/resume_service/service/interfaces/ResumeService.java
+++ src/main/java/com/elevateresume/resume_service/service/interfaces/ResumeService.java
@@ -3,14 +3,16 @@ package com.elevateresume.resume_service.service.interfaces;
 import com.elevateresume.resume_service.dto.ResumeInsertDTO;
 import com.elevateresume.resume_service.dto.ResumeUploadDataDTO;
 import com.elevateresume.resume_service.entity.Resume;
+import com.elevateresume.resume_service.redis.model.ResumeRedisModel;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface ResumeService {
-    Optional<Resume> getResumeByUserId();
+    ResumeRedisModel getResumeByUserId(String userId);
+    ResumeRedisModel getResumeFromDb(String userId);
     List<Resume> getResumesByTemplate(String userId);
     Resume createResume(ResumeInsertDTO resumeInsertDTO);
-    void createResume(ResumeUploadDataDTO resumeUploadDataDTO, MultipartFile file);
+    ResumeRedisModel createResume(ResumeUploadDataDTO resumeUploadDataDTO, MultipartFile file);
 }
