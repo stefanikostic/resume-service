8e8212af091a31da809419d970d2f8219cd22f94
Stefani Kostic <stefanikostic6@gmail.com>
2025-07-08 13:56:08 +0200

    Renamed bulletpoints in SectionDTO.

--- src/main/java/com/elevateresume/resume_service/redis/model/SectionDTO.java
+++ src/main/java/com/elevateresume/resume_service/redis/model/SectionDTO.java
@@ -5,5 +5,5 @@ import com.elevateresume.resume_service.entity.SectionType;
 import java.util.List;
 
 public record SectionDTO(String id, String title, SectionType type, String content, Integer sectionOrder, boolean isOptional,
-                         List<BulletPointDTO> bulletPointModelList, String startPeriod, String endPeriod) {
+                         List<BulletPointDTO> bulletPoints, String startPeriod, String endPeriod) {
 }
