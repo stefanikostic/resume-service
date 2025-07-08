60006061d981b56e8a30d3512b145266339cceba
Stefani Kostic <stefanikostic6@gmail.com>
2025-07-08 13:56:35 +0200

    Fixed too long line in SectionInsertDTO.

--- src/main/java/com/elevateresume/resume_service/dto/SectionInsertDTO.java
+++ src/main/java/com/elevateresume/resume_service/dto/SectionInsertDTO.java
@@ -4,6 +4,7 @@ import com.elevateresume.resume_service.entity.SectionType;
 
 import java.util.List;
 
-public record SectionInsertDTO(SectionType sectionType, String title, String startPeriod, String endPeriod, String content,
+public record SectionInsertDTO(SectionType sectionType, String title, String startPeriod, String endPeriod,
+                               String content,
                                List<BulletPointInsertDTO> bulletPoints) {
 }
